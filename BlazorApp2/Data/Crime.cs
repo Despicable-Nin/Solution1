8953e447d42192ecbbe29c84d5a289e3fc11b400
Ninism <nin.alamo@outlook.com>
2024-09-20 20:41:58 +0800

    init crime

--- BlazorApp2/Data/Crime.cs
+++ BlazorApp2/Data/Crime.cs
@@ -1,4 +1,7 @@
-﻿namespace BlazorApp2.Data
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp2.Data
 {
     public class Crime
     {
@@ -6,23 +9,26 @@
         {
             
         }
-        public int CrimeID { get; set; }
-        public string CrimeType { get; set; }
+
+        [Key]
+        public Guid Id { get; set; }
+        public int CaseID { get; set; }
+        public string? CrimeType { get; set; }
         public DateTime Date { get; set; }
         public TimeSpan Time { get; set; }
-        public string Address { get; set; }
-        public string Severity { get; set; }
-        public string Description { get; set; }
-        public string WeaponUsed { get; set; }
+        public string? Address { get; set; }
+        public string? Severity { get; set; }
+        public string? Description { get; set; }
+        public string? WeaponUsed { get; set; }
         public int VictimCount { get; set; }
-        public string SuspectDescription { get; set; }
+        public string? SuspectDescription { get; set; }
         public bool ArrestMade { get; set; }
         public DateTime? ArrestDate { get; set; } // Nullable for no arrest
         public int ResponseTimeInMinutes { get; set; }
-        public string PoliceDistrict { get; set; }
-        public string WeatherCondition { get; set; }
-        public string CrimeMotive { get; set; }
-        public string NearbyLandmarks { get; set; }
+        public string? PoliceDistrict { get; set; }
+        public string? WeatherCondition { get; set; }
+        public string? CrimeMotive { get; set; }
+        public string? NearbyLandmarks { get; set; }
         public bool RecurringIncident { get; set; }
         public int PopulationDensityPerSqKm { get; set; }
         public double UnemploymentRate { get; set; }
---
+++ BlazorApp2/Data/Migrations/20240920115631_crime-added.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp2.Migrations
+{
+    /// <inheritdoc />
+    public partial class crimeadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Crimes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CaseID = table.Column<int>(type: "int", nullable: false),
+                    CrimeType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Time = table.Column<TimeSpan>(type: "time", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Severity = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WeaponUsed = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VictimCount = table.Column<int>(type: "int", nullable: false),
+                    SuspectDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ArrestMade = table.Column<bool>(type: "bit", nullable: false),
+                    ArrestDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ResponseTimeInMinutes = table.Column<int>(type: "int", nullable: false),
+                    PoliceDistrict = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WeatherCondition = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CrimeMotive = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NearbyLandmarks = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RecurringIncident = table.Column<bool>(type: "bit", nullable: false),
+                    PopulationDensityPerSqKm = table.Column<int>(type: "int", nullable: false),
+                    UnemploymentRate = table.Column<double>(type: "float", nullable: false),
+                    MedianIncome = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ProximityToPoliceStationInKm = table.Column<double>(type: "float", nullable: false),
+                    StreetLightPresent = table.Column<bool>(type: "bit", nullable: false),
+                    CCTVCoverage = table.Column<bool>(type: "bit", nullable: false),
+                    AlcoholOrDrugInvolvement = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Crimes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Crimes");
+        }
+    }
+}
