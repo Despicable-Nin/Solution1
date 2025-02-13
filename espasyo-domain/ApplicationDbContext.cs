ffcf7f59c05591174adaa708271fb6477dce17ba
Ninism <nin.alamo@outlook.com>
2025-02-13 11:55:08 +0800

    Added domain project

---
+++ espasyo-domain/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using espasyo_domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace espasyo_domain
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Incident> Incidents { get; set; }
+    }
+}
---
+++ espasyo-domain/Repositories/IIncidentRepository.cs
@@ -0,0 +1,18 @@
+﻿using espasyo_domain.Entities;
+
+namespace espasyo_domain.Repositories
+{
+    public interface IIncidentRepository
+    {
+        Task<Incident> Create(Incident incident);
+        Task<Incident> Update(Incident incident);
+        Task Delete(Incident incident);
+        Task<Incident> Get(Guid id);
+        IEnumerable<Incident> GetAll(int pageSize, int pageIndex);
+        IEnumerable<Incident> GetByMotive(MotiveEnum motive, KeyValuePair<DateTime, DateTime>? dateRange);
+        IEnumerable<Incident> GetBySeverity(SeverityEnum severity, KeyValuePair<DateTime, DateTime>? dateRange);
+        IEnumerable<Incident> GetByPoliceDistrict(MuntinlupaPoliceDistrictEnum policeDistrict, KeyValuePair<DateTime, DateTime>? dateRange);
+        IEnumerable<Incident> GetByWeather(WeatherConditionEnum weatherCondition,  KeyValuePair<DateTime, DateTime>? dateRange);
+        int GetCount();
+    }
+}
